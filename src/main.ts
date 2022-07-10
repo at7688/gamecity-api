bffb21c5c45a48e13e24661878fa70752597c37a
Summer Chen <mp4bj6ruby@gmail.com>
2022-07-11 00:20:18 +0800

    added https://localhost:xxxx

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:8809',
+      'https://localhost:8809',
       'https://dev.gamecity-admin.techcake.net',
       'https://gamecity-admin.techcake.net',
       'https://gamecity-agent.techcake.net',
