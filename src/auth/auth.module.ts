646eb605363bdbc7d005ff0647a7e4b48b33f80f
Summer Chen <mp4bj6ruby@gmail.com>
2022-07-15 10:35:39 +0800

    added login schedule demo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { TasksService } from 'src/tasks/tasks.service';
 import { UserModule } from 'src/user/user.module';
 import { AgentController } from './agent.controller';
 import { AuthController } from './auth.controller';
@@ -26,6 +27,7 @@ import { JwtStrategy } from './jwt.strategy';
     AuthService,
     JwtStrategy,
     { provide: APP_GUARD, useClass: JwtAuthGuard },
+    TasksService,
   ],
   controllers: [AuthController, AgentController],
 })
