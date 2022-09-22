09d74bebd4bb7c778609bcdf7ae37d26173f1f5f
Summer Chen <mp4bj6ruby@gmail.com>
2022-09-22 16:57:29 +0800

    rm old og

--- src/game-merchant/og_old/types/getBalance.ts
+++
@@ -1,5 +0,0 @@
-export interface OgGetBalanceReq {
-  player_id: string;
-  timestamp: number;
-  signature: string;
-}
--- src/game-merchant/og_old/types/validation.ts
+++
@@ -1,6 +0,0 @@
-export interface OgValidationReq {
-  player_id: string;
-  timestamp: number;
-  token: string;
-  signature: string;
-}
