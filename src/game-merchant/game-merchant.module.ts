8e96ad813beac032051a0fa9af1e1a513c352b6a
Summer Chen <mp4bj6ruby@gmail.com>
2022-09-26 15:02:30 +0800

    rm avia

--- src/game-merchant/game-merchant.module.ts
+++ src/game-merchant/game-merchant.module.ts
@@ -1,16 +1,12 @@
-import { WmController } from './wm/wm.controller';
-import { BngController } from './bng/bng.controller';
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { WalletRecModule } from 'src/wallet-rec/wallet-rec.module';
 import { AbController } from './ab/ab.controller';
 import { AbService } from './ab/ab.service';
 import { AbTaskService } from './ab/ab.task.service';
-import { AviaCbController } from './avia/avia.cb.controller';
-import { AviaCbService } from './avia/avia.cb.service';
-import { AviaController } from './avia/avia.controller';
-import { AviaService } from './avia/avia.service';
-import { AviaTaskService } from './avia/avia.task.service';
+import { BngController } from './bng/bng.controller';
+import { BngService } from './bng/bng.service';
+import { BngTaskService } from './bng/bng.task.service';
 import { BwinController } from './bwin/bwin.controller';
 import { BwinService } from './bwin/bwin.service';
 import { BwinTaskService } from './bwin/bwin.task.service';
@@ -21,15 +17,14 @@ import { GrTaskService } from './gr/gr.task.service';
 import { OgController } from './og/og.controller';
 import { OgService } from './og/og.service';
 import { OgTaskService } from './og/og.task.service';
-import { PlatformsBridgeService } from './platforms-bridge/platforms-bridge.service';
 import { PlatformsBridgeController } from './platforms-bridge/platforms-bridge.controller';
-import { ZgService } from './zg/zg.service';
-import { ZgTaskService } from './zg/zg.task.service';
-import { ZgController } from './zg/zg.controller';
-import { BngService } from './bng/bng.service';
-import { BngTaskService } from './bng/bng.task.service';
+import { PlatformsBridgeService } from './platforms-bridge/platforms-bridge.service';
+import { WmController } from './wm/wm.controller';
 import { WmService } from './wm/wm.service';
 import { WmTaskService } from './wm/wm.task.service';
+import { ZgController } from './zg/zg.controller';
+import { ZgService } from './zg/zg.service';
+import { ZgTaskService } from './zg/zg.task.service';
 
 @Module({
   imports: [WalletRecModule, AuthModule],
@@ -39,8 +34,6 @@ import { WmTaskService } from './wm/wm.task.service';
     WmController,
     ZgController,
     OgController,
-    AviaController,
-    AviaCbController,
     BwinController,
     GrController,
     PlatformsBridgeController,
@@ -56,9 +49,6 @@ import { WmTaskService } from './wm/wm.task.service';
     ZgTaskService,
     OgService,
     OgTaskService,
-    AviaService,
-    AviaCbService,
-    AviaTaskService,
     BwinService,
     BwinTaskService,
     GrService,
